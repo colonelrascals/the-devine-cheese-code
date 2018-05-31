0428f416d84ef1e724dbbbd4833d6e7678260f3f
colonelrascals <pdhildreth@gmail.com>
2018-05-31 17:23:00 -0500

    added attional ns

--- src/the_devine_cheese_code/core.clj
+++ src/the_devine_cheese_code/core.clj
@@ -1,7 +1,36 @@
 (ns the-devine-cheese-code.core
   (:gen-class))
+(require 'the-devine-cheese-code.visualization.svg)
+
+(refer 'the-devine-cheese-code.visualization.svg)
+
+(def heists [{:location "Cologne, Germany"
+              :cheese-name "Cheese Pretzel"
+              :lat 50.95
+              :lng 6.97}
+             {:location "Zurich, Switzerland"
+              :cheese-name "The Standard Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Marseille, France"
+              :cheese-name "Le Fromage de Cosquer"
+              :lat 43.30
+              :lng 5.37}
+             {:location "Zurich, Switzerland"
+              :cheese-name "The Lesser Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Vatican City (The Holy Sea)"
+              :cheese-name "The Cheese of Turin"
+              :lat 41.90
+              :lng 12.45}])
+             
+
+             
+  
+
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (points heists)))
