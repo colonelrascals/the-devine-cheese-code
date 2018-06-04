84bd844e5d8bcf8381ed539177cfbc9fdd5ea476
colonelrascals <pdhildreth@gmail.com>
2018-06-04 17:20:18 -0500

    coding coding coding

--- src/the_devine_cheese_code/visualization/svg.clj
+++ src/the_devine_cheese_code/visualization/svg.clj
@@ -1,4 +1,6 @@
-(ns the-devine-cheese-code.visualization.svg)
+(ns the-devine-cheese-code.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
 
 (defn latlng->point 
   [latlng]
@@ -8,5 +10,24 @@
   [locations]
   (clojure.string/join " " (map latlng->point locations)))
    
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+      (map (fn [k] (apply comparison-fn (map k maps)))
+       ks))))  
   
-  
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
+
+(defn translate-to-00
+  [locations]
+  (let [mincooords (min locations)]
+    (map #(merge-with - % mincooords) locations)))
+
+(defn scale
+  [width height locations]
+  (let [maxcoords (max locations)
+        ratio {:lat (/ height (:lat maxcoords))
+               :lng (/ width (:lng maxcoords))}]
+      (map #(merge-with * % ratio) locations)))
